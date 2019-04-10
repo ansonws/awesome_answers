1999534ff0504b92b6d577008d9843b0dc1d30ee
Anson Lam <anson.lam94@gmail.com>
2019-04-10 13:18:55 -0700

    Refactor questions controller and new/edit view

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,8 +1,49 @@
 class QuestionsController < ApplicationController
+    # Order matters for before_action
+    before_action :find_question, only: [:show, :edit, :update]
+    before_action :authenticate_user 
 
     def new
         @question = Question.new
     end
+
+    def create
+        @question = Question.new question_params
+        if @question.save
+            # The redirect_to method is used for telling the browser to make a new request.
+            # The redirect_to method is typically used with a named route helper.
+            redirect_to question_path(@question.id) # Omitting .id also works in rails
+        else
+            render :new 
+        end
+    end
+        
+    def show
+    end
+
+    def index
+        @questions = Question.all.order(created_at: :desc)
+    end
+    
+    def edit
+    end
+    
+    def update
+        question_params =  params.require(:question).permit(:title, :body)
+        if @question.update question_params
+            redirect_to question_path(@question)
+        else
+            render :edit
+        end
+    end
+
+    def destroy
+        question = Question.find(params[:id])
+        question.destroy
+        redirect_to root_path
+    end
+
+    private
 
     def create
         # The 'params' object available in controllers is composed of the query string, url params, and the body of a form
@@ -14,42 +55,12 @@ class QuestionsController < ApplicationController
         # Use 'require' on the params object to retrieve the nested hash of a key usually corresponding to the name-value pairs of a form.
         # Then use permit to specify all input names that are allowable as symbols.
         question_params =  params.require(:question).permit(:title, :body)
-        @question = Question.new question_params
-        if @question.save
-            # The redirect_to method is used for telling the browser to make a new request.
-            # The redirect_to method is typically used with a named route helper.
-            redirect_to question_path(@question.id) # Omitting .id also works in rails
-        else
-            render :new 
-        end
-        
-        def show
-            @question = Question.find(params[:id])
-        end
-
-        def index
-            @questions = Question.all.order(created_at: :desc)
-        end
-        
-        def edit
-            @question = Question.find(params[:id])
-        end
-        
-        def update
-            @question = Question.find(params[:id])
-            question_params =  params.require(:question).permit(:title, :body)
-            if @question.update question_params
-                redirect_to question_path(@question)
-            else
-                render :edit
-            end
-        end
-
-        def destroy
-            question = Question.find(params[:id])
-            question.destroy
-            redirect_to root_path
-        end
     end
 
+    def question_params
+        # Use 'require' on the params object to retrieve the nested hash of a key usually corresponding to the name-value pairs of a form
+    
+        # Then use permit to specify all input names that are allowable (as symbols).
+        params.require(:question).permit(:title, :body)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,21 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  # This defines a 'route' rule that says when we recieve a 'GET' request with URL '/', handle it using the 'WelcomeController' with 'index' action inside that controller
-  get("/", { to: 'welcome#index', as: 'root' } )
-
-  get('/contacts/new', { to: 'contact#new' })
-
-  post('/contacts', { to: 'contact#create' })
-
-  get('/questions/new', { to: 'questions#new', as: :new_question})
-
+  get('/questions/new', to: 'questions#new', as: :new_question)
   post('/questions', to: 'questions#create', as: :questions)
-
   get('/questions/:id', to: 'questions#show', as: :question)
   # question_path(<id>), question_url(<id>)
   get('/questions/:id/edit', to: 'questions#edit', as: :edit_question)
   # edit_question_path(<id>), edit_question_url(<id>)
-  patch('/questions/:id', to: 'questions#update')
+  patch("/questions/:id", to: "questions#update")
+  delete('/questions/:id', to: 'questions#destroy')
+  get('/questions', to: 'questions#index')
+
+  post('/contacts', to: 'contact#create' )
+  get('/contacts/new', to: 'contact#new' )
+
+  # this defines a `route` rule that says when we recieve a `GET` request with URL `/`, handle it using the `WelcomeController` with `index` action inside that controller
+  # The `as` option is used for helper url/path, it overrides or generates helper method that you can use in your views or controllers
+
+  get('/', { to: "welcome#index", as: 'root'} )
 end
